bbef72a397f264b33673ae5bc5209491d0e175bc
JoMingyu <city7310@naver.com>
2018-02-11 00:32:47 +0900

    [DONE] school signup request API, signup status API

--- Server/app/docs/account/signup.py
+++ Server/app/docs/account/signup.py
@@ -15,6 +15,28 @@ SIGNUP_POST = {
             'in': 'json',
             'type': 'str',
             'required': True
+        }
+    ],
+    'responses': {
+        '201': {
+            'description': '회원가입 성공',
+        },
+        '204': {
+            'description': 'ID 중복으로 인해 회원가입 실패'
+        }
+    }
+}
+
+SCHOOL_SIGNUP_POST = {
+    'tags': ['가입 요청'],
+    'description': '학교에 가입 요청',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token(JWT ***)',
+            'in': 'header',
+            'type': 'str',
+            'required': True
         },
         {
             'name': 'schoolId',
@@ -33,13 +55,44 @@ SIGNUP_POST = {
     ],
     'responses': {
         '201': {
-            'description': '회원가입 성공, 추가 정보를 입력하는 API를 이용해 나머지 정보를 올려 주어야 함.',
+            'description': '가입 요청 성공',
         },
         '204': {
-            'description': 'ID 중복으로 인해 회원가입 실패'
-        },
-        '205': {
             'description': '존재하지 않는 school id'
+        }
+    }
+}
+
+SCHOOL_SIGNUP_GET = {
+    'tags': ['가입 요청'],
+    'description': '자신의 가입 요청 상태 확인',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token(JWT ***)',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': """
+            가입 요청 상태 확인 성공. requested는 자신이 가입 요청을 보냈는지(true: 보냄, false: 보내지 않음)를 의미하며
+            signed는 자신의 학교 가입 상태를 의미합니다.(true: 가입됨, false: 가입되지 않음)
+            - 가능한 조합은
+            requested: true, signed: true = 가입 요청을 보냈으며, 승인받아 가입된 상태
+            requested: true, signed: false = 가입 요청을 보냈으며, 관리자가 아직 처리하지 않은 상태
+            requested: false, signed: false = 가입 요청을 보내지 않았고, 가입되지 않은 상태
+            requested: false, signed: true = 해당 경우 없음
+            """,
+            'examples': {
+                '': {
+                    'schoolName': '대덕소프트웨어마이스터고등학교',
+                    'requested': True,
+                    'signed': None
+                }
+            }
         }
     }
 }
--- Server/app/views/account/signup.py
+++ Server/app/views/account/signup.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, Response
+from flask_jwt_extended import get_jwt_identity
 from flask_restful import Api, request
 from flasgger import swag_from
 from werkzeug.security import generate_password_hash
@@ -6,7 +7,7 @@ from werkzeug.security import generate_password_hash
 from app.docs.account.signup import *
 from app.models.account import AccountModel
 from app.models.school import SchoolModel
-from app.views import BaseResource, json_required
+from app.views import BaseResource, auth_required, json_required
 
 api = Api(Blueprint('account-signup-api', __name__))
 
@@ -21,33 +22,67 @@ class Signup(BaseResource):
         """
         id = request.json['id']
         pw = request.json['pw']
-        school_id = request.json['schoolId']
-        admission_year = request.json['admissionYear']
 
         hashed_pw = generate_password_hash(pw)
 
         if AccountModel.objects(id=id).first():
             return Response('', 204)
 
-        if len(school_id) != 24:
-            return Response('', 205)
-
-        school = SchoolModel.objects(school_id=school_id).first()
-        if not school:
-            return Response('', 205)
-
-        if AccountModel.objects(id=id, school=school).count() == 0:
-            # 해당 학교의 첫 가입자가 관리자
-            is_admin = accepted = True
-        else:
-            is_admin = accepted = False
-
         AccountModel(
             id=id,
-            pw=hashed_pw,
-            is_admin=is_admin,
-            accepted=accepted,
-            admission_year=admission_year
+            pw=hashed_pw
         ).save()
 
         return Response('', 201)
+
+
+@api.resource('/signup/school')
+class SchoolSignup(BaseResource):
+    @swag_from(SCHOOL_SIGNUP_POST)
+    @auth_required
+    @json_required
+    def post(self):
+        """
+        학교에 가입 요청
+        """
+        school_id = request.json['schoolId']
+        admission_year = request.json['admissionYear']
+
+        if len(school_id) != 24:
+            return Response('', 204)
+
+        school = SchoolModel.objects(school_id=school_id).first()
+        if not school:
+            return Response('', 204)
+
+        if AccountModel.objects(school=school).count() == 0:
+            # 해당 학교의 첫 가입자가 관리자
+            is_admin = True
+        else:
+            is_admin = False
+
+        user = AccountModel.objects(id=get_jwt_identity()).first()
+        user.update(
+            is_admin=is_admin,
+            requested=True,
+            signed=True if is_admin else False,
+            school=school,
+            admission_year=admission_year
+        )
+
+        return Response('', 201)
+
+    @swag_from(SCHOOL_SIGNUP_GET)
+    @auth_required
+    def get(self):
+        """
+        자신의 학교 가입 상태 확인
+        """
+        user = AccountModel.objects(id=get_jwt_identity()).first()
+
+        return {
+            'schoolName': user.school.name,
+            'requested': user.requested,
+            'signed': user.signed
+            # null(None)은 아직 처리되지 않음, True는 수락, False는 거절됨
+        }
