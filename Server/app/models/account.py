2d7bde57007a8bf48ab975adfc7fa3cce28a5598
JoMingyu <city7310@naver.com>
2018-02-11 02:11:23 +0900

    [UPDATE] Enhance AccountModel
    - Add comments
    - Add required=True, default=False to is_admin field

--- Server/app/models/account.py
+++ Server/app/models/account.py
@@ -5,6 +5,9 @@ from app.models.school import SchoolModel
 
 
 class AccountModel(Document):
+    """
+    사용자 계정을 다루는 모델
+    """
     meta = {
         'collection': 'account'
     }
@@ -24,22 +27,31 @@ class AccountModel(Document):
         required=True
     )
 
-    is_admin = BooleanField()
+    is_admin = BooleanField(
+        required=True,
+        default=False
+    )
+    # 사용자의 관리자 여부
 
     requested = BooleanField(
         required=True,
         default=False
     )
     # 가입 요청을 이미 보냈는지의 여부
+
     signed = BooleanField(
         required=True,
         default=False
     )
+    # 학교 가입 상태
 
     school = ReferenceField(
         document_type=SchoolModel
     )
+    # 가입 요청을 보냈거나, 가입되어 있는 학교
+
     admission_year = IntField()
+    # 입학 연도
 
 
 class RefreshTokenModel(Document):
