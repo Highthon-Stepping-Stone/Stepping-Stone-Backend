650b852a611abbf6de91500b0ca3833e7f4b8d79
JoMingyu <city7310@naver.com>
2018-02-10 23:31:16 +0900

    [UPDATE] Renew SchoolModel - school - scheduled album/free album - folder - image list

--- Server/app/models/school.py
+++ Server/app/models/school.py
@@ -6,6 +6,61 @@ class SchoolModel(Document):
         'collection': 'school'
     }
 
+    school_id = StringField(
+        primary_key=True
+    )
+
+    web_url = StringField(
+        required=True
+    )
+
     name = StringField(
         required=True
+    )
+
+    schedules = DictField(
+        # {'YYYY-MM-DD': ['', '', ''], ...}
+        # default={}
+    )
+
+
+class AlbumBase(Document):
+    meta = {
+        'abstract': True,
+        'allow_inheritance': True
+    }
+
+    school = ReferenceField(
+        document_type=SchoolModel,
+        required=True
+    )
+
+
+class ScheduledAlbumModel(AlbumBase):
+    meta = {
+        'collection': 'school_calendar_album'
+    }
+
+
+class FreeAlbumModel(AlbumBase):
+    meta = {
+        'collection': 'free_album'
+    }
+
+
+class FolderModel(Document):
+    meta = {
+        'collection': 'folder'
+    }
+
+    album = ReferenceField(
+        document_type=AlbumBase,
+        required=True
+    )
+
+    image_names = ListField(
+        StringField(
+            required=True
+        )
+        # default={}
     )
