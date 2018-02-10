59ee394f0bb02315b179a8b4ae67cac21fcd2cb2
JoMingyu <city7310@naver.com>
2018-02-11 00:32:58 +0900

    [DONE] School signup approve API

---
+++ Server/app/docs/account/approve.py
@@ -0,0 +1,97 @@
+APPROVE_SIGNUP_GET = {
+    'tags': ['가입 승인'],
+    'description': '가입 승인 요청자 목록 조회',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token(JWT ***)',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '가입 승인 요청자 조회 성공',
+            'examples': {
+                '': [
+                    {
+                        'id': 'planb',
+                        'admissionYear': 2017
+                    },
+                    {
+                        'id': 'planb2',
+                        'admissionYear': 2016
+                    }
+                ]
+            }
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
+
+APPROVE_SIGNUP_POST = {
+    'tags': ['가입 승인'],
+    'description': '가입 요청 승인',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token(JWT ***)',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'id',
+            'description': '가입을 승인할 사용자의 ID',
+            'in': 'json',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '가입 승인 성공'
+        },
+        '204': {
+            'description': '해당 ID를 가진 사용자 없음'
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
+
+APPROVE_SIGNUP_DELETE = {
+    'tags': ['가입 승인'],
+    'description': '가입 요청 거절',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token(JWT ***)',
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+        {
+            'name': 'id',
+            'description': '가입을 거절할 사용자의 ID',
+            'in': 'json',
+            'type': 'str',
+            'required': True
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '가입 거절 성공'
+        },
+        '204': {
+            'description': '해당 ID를 가진 사용자 없음'
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
---
+++ Server/app/views/account/approve.py
@@ -0,0 +1,66 @@
+from flask import Blueprint, Response
+from flask_jwt_extended import get_jwt_identity
+from flask_restful import Api, abort, request
+from flasgger import swag_from
+
+from app.docs.account.approve import *
+from app.models.account import AccountModel
+from app.views import BaseResource
+
+api = Api(Blueprint('account-approve-api', __name__))
+
+
+@api.resource('/signup/approve')
+class ApproveSignup(BaseResource):
+    @swag_from(APPROVE_SIGNUP_GET)
+    def get(self):
+        """
+        가입 승인 요청자 목록 조회
+        """
+        user = AccountModel.objects(id=get_jwt_identity()).first()
+
+        if not user.is_admin:
+            abort(403)
+
+        return self.unicode_safe_json_response([{
+            'id': user.id,
+            'admissionYear': user.admission_year
+        } for user in AccountModel.objects(accepted=False, school=user.school)])
+
+    @swag_from(APPROVE_SIGNUP_POST)
+    def post(self):
+        """
+        가입 요청 승인
+        """
+        user = AccountModel.objects(id=get_jwt_identity()).first()
+
+        if not user.is_admin:
+            abort(403)
+
+        id = request.json['id']
+
+        user = AccountModel.objects(id=id).first()
+
+        if not user:
+            return Response('', 204)
+
+        user.update(accepted=True)
+
+        return Response('', 200)
+
+    @swag_from(APPROVE_SIGNUP_DELETE)
+    def delete(self):
+        """
+        가입 요청 거절
+        """
+        user = AccountModel.objects(id=get_jwt_identity()).first()
+
+        if not user.is_admin:
+            abort(403)
+
+        id = request.json['id']
+
+        user = AccountModel.objects(id=id).first()
+
+        if not user:
+            return Response('', 204)
