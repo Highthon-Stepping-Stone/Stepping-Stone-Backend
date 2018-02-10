3b9bbc0e8db27c8ac544b2f026d090341135d20d
JoMingyu <city7310@naver.com>
2018-02-10 20:13:26 +0900

    [DONE] SNS auth, refresh API

--- Server/app/views/account/auth.py
+++ Server/app/views/account/auth.py
@@ -43,3 +43,58 @@ class Auth(BaseResource):
             'accessToken': create_access_token(id),
             'refreshToken': create_refresh_token(str(refresh_token))
         }, 200
+
+
+@api.resource('/auth/sns')
+class SNSAuth(BaseResource):
+    @swag_from(SNS_AUTH_POST)
+    @json_required
+    def post(self):
+        """
+        SNS 계정으로 로그인
+        """
+        id = request.json['id']
+        connected_service = request.json['connected_service']
+
+        user = SNSAccountModel.objects(id=id).first()
+        if not user:
+            user = SNSAccountModel(
+                id=id,
+                connected_service=connected_service
+            ).save()
+
+            status_code = 201
+        else:
+            status_code = 200
+
+        refresh_token = uuid4()
+        RefreshTokenModel(
+            token=refresh_token,
+            token_owner=user
+        ).save()
+        # Generate new refresh token made up of uuid4
+
+        return {
+            'accessToken': create_access_token(id),
+            'refreshToken': create_refresh_token(str(refresh_token))
+        }, status_code
+
+
+@api.resource('/refresh')
+class Refresh(BaseResource):
+    @swag_from(REFRESH_GET)
+    @jwt_refresh_token_required
+    def get(self):
+        """
+        새로운 Access Token 획득
+        """
+        token = RefreshTokenModel.objects(token=get_jwt_identity()).first()
+
+        if not token or token.token_owner.pw != token.pw_snapshot:
+            # Invalid token or the token issuing password is different from the current password
+            # Returns status code 205 : Reset Content
+            return Response('', 205)
+
+        return {
+            'accessToken': create_access_token(token.token_owner.id)
+        }, 200
