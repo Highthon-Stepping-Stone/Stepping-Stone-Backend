3e8d40e83cce6a7b5a380a57ac28e4f2f51b22aa
JoMingyu <city7310@naver.com>
2018-02-11 09:41:19 +0900

    [UPDATE] Add school initializing

--- Server/server.py
+++ Server/server.py
@@ -1,7 +1,11 @@
 from argparse import ArgumentParser
 import os
 
+from app.models.album import FreeAlbumModel, ScheduledAlbumModel
+from app.models.school import SchoolModel
 from app import app
+
+from utils import school_parser
 
 if __name__ == '__main__':
     if 'SECRET_KEY' not in os.environ:
@@ -12,8 +16,11 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--port')
     args = parser.parse_args()
 
-    from utils import school_parser
-    school_parser.parse_school_list_from_excel()
+    school = SchoolModel(school_id='G100000170', web_url='stu.dje.go.kr', name='대덕소프트웨어마이스터고등학교').save()
+    ScheduledAlbumModel(school=school).save()
+    FreeAlbumModel(school=school).save()
+
     school_parser.parse_school_schedules('1234')
+    # TODO 나이스 문제 없어지면 하드코딩 제거해야 함
 
     app.run(host=app.config['HOST'], port=int(args.port) if args.port else app.config['PORT'], debug=app.debug, threaded=True)
